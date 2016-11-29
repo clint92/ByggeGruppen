1d9795a7d18070e6df338d776393ecc36f2c2e04
“clint92” <clin0001@stud.kea.dk>
2016-11-29 11:39:18 +0100

    fxmlUpdate

--- src/BusinessLogic/Admin.java
+++ src/BusinessLogic/Admin.java
@@ -1,9 +1,19 @@
 package BusinessLogic;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.Observable;
+import java.util.ResourceBundle;
+
 /**
  * Created by Jennes on 25-11-2016.
  */
-public class Admin extends User {
+public class Admin extends User implements Initializable {
     private int userid = 1;
 
     public void setUserid(int id){
@@ -13,4 +23,15 @@ public class Admin extends User {
     public int getUserid(){
         return this.userid;
     }
+
+
+    @FXML
+    public ComboBox<String> userType;
+    ObservableList<String> types = FXCollections.observableArrayList("Client", "Contractor", "Admin");
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    userType.setItems(types);
+    }
+
 }
