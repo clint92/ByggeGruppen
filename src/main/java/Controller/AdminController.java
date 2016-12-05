c9d7eb8b2fb6eba9fd1c0a68350395da4ab83230
“clint92” <clin0001@stud.kea.dk>
2016-12-05 17:38:49 +0100

    added exception

--- src/main/java/Controller/AdminController.java
+++ src/main/java/Controller/AdminController.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import BusinessLogic.Admin;
+import BusinessLogic.EmptyFieldException;
 import BusinessLogic.User;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -28,8 +29,6 @@ public class AdminController extends Controller implements Initializable {
     public TextField number;
 
 
-
-
     @FXML
     public ComboBox<String> usertype;
     ObservableList<String> types = FXCollections.observableArrayList("Client", "Contractor", "Admin");
@@ -39,23 +38,31 @@ public class AdminController extends Controller implements Initializable {
         usertype.setItems(types);
     }
 
-    public void createProfile()
-    {
-        String type = usertype.getSelectionModel().getSelectedItem().toString();
+    public void createProfile() throws EmptyFieldException {
         int level = -1;
-        if(type ==  "Client")
-        {
-            level = 2;
+        try {
+            if (usertype.getValue() != null) {
+                String type = usertype.getSelectionModel().getSelectedItem().toString();
+                if (type.equals("Client")) {
+                    level = 2;
+                } else if (type.equals("Contractor")) {
+                    level = 1;
+                } else if (type.equals("Admin")) {
+                    level = 0;
+                }
+            } else {
+                throw new EmptyFieldException();
+            }
+            if (usernameboks.getText().equals("") || passwordboks.getText().equals("") || level == -1 || firstname.getText().equals("") || lastname.getText().equals("") || address.getText().equals("") || zip.getText().equals("") || email.getText().equals("") || number.getText().equals("")) {
+                System.out.println("fejl");
+                throw new EmptyFieldException();
+            } else {
+                newU.addProfile(usernameboks.getText(), passwordboks.getText(), level, firstname.getText(), lastname.getText(), address.getText(), Integer.parseInt(zip.getText()), email.getText(), Integer.parseInt(number.getText()));
+                System.out.println(firstname.getText());
+            }
+        } catch (EmptyFieldException e) {
+            e.printStackTrace();
         }
-        else if(type == "Contractor")
-        {
-            level = 1;
-        }
-        else if(type == "Admin")
-        {
-            level = 0;
-        }
-        newU.addProfile(usernameboks.getText(), passwordboks.getText(), level, firstname.getText(), lastname.getText(), address.getText(), Integer.parseInt(zip.getText()), email.getText(), Integer.parseInt(number.getText()));
-        //System.out.println(usernameboks.getText() + passwordboks.getText() + level );
     }
+
 }
