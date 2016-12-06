1f18b94c056115bf1b6709391c4c471db4a8fff2
JasperHessellund <jasperhessellund@gmail.com>
2016-12-06 15:58:13 +0100

    getProjectDate

--- src/main/java/Controller/ProjektInitialize.java
+++ src/main/java/Controller/ProjektInitialize.java
@@ -1,21 +1,56 @@
 package Controller;
 
-import BusinessLogic.MyProject;
-import BusinessLogic.User;
+import BusinessLogic.*;
 import javafx.fxml.Initializable;
-import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.util.StringConverter;
+
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
-/**
- * Created by Julius on 06-12-2016.
- */
 public class ProjektInitialize extends Controller implements Initializable {
+
+    @FXML
+    public DatePicker calender;
+    Timeline tl = new Timeline();
+    MyCalender mp = new MyCalender();
+    String pattern = "dd/MM/yyyy";
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         timeline.setVvalue(1.0);
-        timeline.setContent(TL.getTimeline(MyProject.projectInstance().getProjectName()));
+        timeline.setContent(tl.getTimeline(MyProject.projectInstance().getProjectName()));
+        calender.setConverter(new StringConverter<LocalDate>() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(pattern);
+
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
+
+        });
+        calender.setOnAction(e-> getProjectDate());
+    }
+
+    public void getProjectDate(){
+        timeline.setContent(mp.getProjectDate(calender.getEditor().getText()));
     }
 }
