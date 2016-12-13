f99c0fe002ea7ea2dc822e8a8088b1e593cfb419
JasperHessellund <jasperhessellund@gmail.com>
2016-12-13 20:01:31 +0100

    LastEditsV1.0

--- src/main/java/Database/MyBackup.java
+++ src/main/java/Database/MyBackup.java
@@ -1,12 +1,18 @@
 package Database;
 
 
-public class MyBackup implements Backup {
-    @Override
-    public void makeBackup() {
+import java.io.IOException;
 
+public class MyBackup implements Backup {
+
+    public void makeBackup() throws IOException {
+        try {
+            Runtime rt = Runtime.getRuntime();
+            rt.exec("C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\mysqldump -u sql7146768 -pw2gAY97YQQ  sql7146768 -r  D:\\BackupTest.sql");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    @Override
     public void getBackup() {
 
     }
